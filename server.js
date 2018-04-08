4d81006aa9377deff3613cc6a51650c1695c1b02
dvsocean <dvsocean@icloud.com>
2018-04-08 16:13:30 -0700

    rerun deploy model

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet Jason for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Pickup microchips from store',
+	completed: false
+}, {
+	id: 3,
+	description: 'Re-run the deployment model',
+	completed: false
+}];
 
 app.get('/', function(req, res){
 	res.send('Todo API ROOT');
 });
+
+//GET REQUEST FOR /todos
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+//GET INDIVIDUAL TODOS /todos/id
 
 app.listen(PORT, function(){
 	console.log('Express listening on port number ' + PORT + '!');

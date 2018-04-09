9b6d2e8b17bf0f5c5d07e3f84ec98bf048b1a967
dvsocean <dvsocean@icloud.com>
2018-04-08 17:21:23 -0700

    updates to todo app

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
+
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet Jason for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Pickup microchips from store',
-	completed: false
-}, {
-	id: 3,
-	description: 'Re-run the deployment model',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Todo API ROOT');
@@ -23,6 +17,7 @@ app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
+//GET /todos
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
 	var found;
@@ -36,9 +31,40 @@ app.get('/todos/:id', function(req, res){
 	} else {
 		res.status(404).send();
 	}
-	// res.send('Asking for todo with id of: ' + req.params.id);
 });
+
+//POST /todos
+app.post('/todos', function(req, res){
+	var body = req.body;
+
+// ADD ID FIELD
+	body.id = todoNextId++;
+//PUSH INTO ARRAY
+	todos.push(body);
+
+	console.log('description ' + body.description);
+	res.json(body);
+});
+
 
 app.listen(PORT, function(){
 	console.log('Express listening on port number ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

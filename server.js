272601709b32f57cf7517f3d372dc64c3cc42e31
dvsocean <dvsocean@icloud.com>
2018-04-09 20:34:19 -0700

    updates by underscore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,12 +20,15 @@ app.get('/todos', function(req, res){
 //GET /todos
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var found;
-	todos.forEach(function(todo){
-		if(todo.id === todoId){
-			found = todo;
-		}
-	});
+	var found = _.findWhere(todos, {id: todoId});
+
+	// var found;
+	// todos.forEach(function(todo){
+	// 	if(todo.id === todoId){
+	// 		found = todo;
+	// 	}
+	// });
+
 	if (found) {
 		res.json(found);
 	} else {
@@ -35,7 +38,13 @@ app.get('/todos/:id', function(req, res){
 
 //POST /todos
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	//set body.des to trimmed value
+	body.description = body.description.trim();
 
 // ADD ID FIELD
 	body.id = todoNextId++;

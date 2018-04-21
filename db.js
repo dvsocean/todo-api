5227327114458cb7f61a52a07fd2cb2ba2c02b13
dvsocean <dvsocean@icloud.com>
2018-04-21 15:08:34 -0700

    updates for restful-ocean

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
+var connectionString = "postgres://hqiutrclpopfni:db73d00de1328afec9d5d0d2f7c7841ac94c02861adbbfc20565150852fa8477@ec2-54-163-240-54.compute-1.amazonaws.com:5432/d2dl6j940g6gt0";
 var sequelize;
 
 if (env === 'production') {
-	sequelize = new Sequelize(process.env.DATABASE_URL, {
+	sequelize = new Sequelize(connectionString, {
 		dialect: 'postgres'
 	});
 } else {
